8d6fb38662673e7d637238819b760c291f23016a
Vishal Patidar <er.vishalpatidar29@gmail.com>
2025-09-15 19:43:33 +0530

    create the logout functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const cookieParser = require("cookie-parser");
 const path = require('path');
+const expressSession = require('express-session');
+const flash = require('connect-flash');
 
 const ownersRouter = require("./routes/ownersRouter");
 const usersRouter = require("./routes/usersRouter");
@@ -17,9 +19,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
+app.use( expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET
+}));
+app.use(flash());
 app.set("view engine", "ejs");
 
-// app.use("/", indexRouter);
+app.use("/", indexRouter);
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,55 +1,56 @@
 const userModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { generateToken } = require('../utils/generateToken');
- 
-module.exports.registerUser =   async function (req, res) {
-    try {
-        let { email, password, fullname } = req.body;
+const { generateToken } = require("../utils/generateToken");
 
-        let user = await userModel.findOne({email : email});
-        if(user) return res.status(401).send("User Already Have Account. Please Login !");
+module.exports.registerUser = async function (req, res) {
+  try {
+    let { email, password, fullname } = req.body;
 
-        bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(password, salt, async function (err, passwordhash) {
+    let user = await userModel.findOne({ email: email });
+    if (user)
+      return res.status(401).send("User Already Have Account. Please Login !");
 
-            if (err) {
-            return res.send(err.message);
-            } else {
-            let user = await userModel.create({
-                email,
-                password: passwordhash,
-                fullname,
-            });
+    bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(password, salt, async function (err, passwordhash) {
+        if (err) {
+          return res.send(err.message);
+        } else {
+          let user = await userModel.create({
+            email,
+            password: passwordhash,
+            fullname,
+          });
 
-            let token = generateToken(user);
-            res.cookie("token", token);
-            res.send("user Created Successfully !");
-            }
-        });
-        });
-    } catch (err) {
-        console.log(err.message);
-    }
+          let token = generateToken(user);
+          res.cookie("token", token);
+          res.send("user Created Successfully !");
+        }
+      });
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 module.exports.loginUser = async function (req, res) {
-    
-    let {email, password} = req.body;
-   let user = await userModel.findOne({ email: email});
+  let { email, password } = req.body;
+  let user = await userModel.findOne({ email: email });
 
-   if(!user) return res.send("Email or Password incorrect");
+  if (!user) return res.send("Email or Password incorrect");
 
-   bcrypt.compare(password, user.password, function (err, result) {
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) {
+      let token = generateToken(user);
+      res.cookie("token", token);
+      res.send("Login Successfully !");
+    } else {
+      res.send("Email or Password incorrect");
+    }
+  });
+};
 
-   if(result){
-    let token =  generateToken(user);
-    res.cookie("token", token);
-    res.send("Login Successfully !");
-
-   }else{
-    res.send("Email or Password incorrect");
-   }
-
-   });
+module.exports.logout = async function (req, res) {
+  res.cookie("token", "");
+  res.redirect("/");
 };
